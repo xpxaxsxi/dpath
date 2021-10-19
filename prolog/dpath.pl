4034bb02a9c1af81c7e376d83797ebe404358c24
xpxaxsxi <pasikuuskasi@windowslive.com>
2021-10-19 20:44:48 +0200

    removed commented out code ...
    ... that was used in some phase of development

--- prolog/dpath.pl
+++ prolog/dpath.pl
@@ -117,15 +117,11 @@ filetype(NotCompound):-
 
 
 filetype( Drive:/Path):-
-          %compound(DP),
-          %DP=Drive :/ Path,
           atom(Drive),!,
           atom_concat(Drive,':',DriveAtom),
           filetype(DriveAtom/Path).
 
 filetype( Drive:/Path):-
-          %compound(DP),
-          %DP=Drive :/ Path,
           var(Drive),
           !,
           member(Drive,[c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]),
